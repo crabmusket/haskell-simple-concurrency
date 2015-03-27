8da14fe2f5310a51eb7fe4cf0c6c789e078c57cb
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-03-28 08:58:21 +1100

    Add examples to Main that had been forgotten.

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,24 @@
--- | Main entry point to the application.
+-- Main entry point to the application.
 module Main where
 
 import qualified Ex1Threads (main)
 import qualified Ex2MVars (main)
 import qualified Ex3Channels (main)
+import qualified Ex4DirectedChannels (main)
 import qualified Ex5Select (main)
 import qualified Ex6Timeouts (main)
+import qualified Ex7ComposableSelect (main)
 
--- | The main entry point.
+-- The main entry point.
 main :: IO ()
 main = do
-    --example "Threads" Ex1Threads.main
-    --example "MVar" Ex2MVars.main
-    --example "Channels" Ex3Channels.main
-    --example "Select" Ex5Select.main
+    example "Threads" Ex1Threads.main
+    example "MVar" Ex2MVars.main
+    example "Channels" Ex3Channels.main
+    example "Directed channels" Ex4DirectedChannels.main
+    example "Select" Ex5Select.main
     example "Timeouts" Ex6Timeouts.main
+    example "Composable select" Ex7ComposableSelect.main
 
 example title code = do
     putStrLn ("~~~ Running " ++ title ++ " example! ~~~")
