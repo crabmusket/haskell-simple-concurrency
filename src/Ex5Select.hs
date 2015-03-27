e3946678821cb0befe08694c6399445adee9855e
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-03-26 19:40:19 -0700

    Added select example.

---
+++ src/Ex5Select.hs
@@ -0,0 +1,36 @@
+module Ex5Select where
+
+import Ex1Threads (sleepMs)
+
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
+import Control.Monad (forM)
+import System.Random (randomRIO)
+
+main = do
+    item1 <- newEmptyMVar
+    item2 <- newEmptyMVar
+
+    putStrLn "Let the race begin!"
+    forkIO (worker "Harry" item1)
+    forkIO (worker "Sally" item2)
+
+    name <- select [item1, item2]
+    putStrLn (name ++ " finished first!")
+
+select vars = do
+    won <- newEmptyMVar
+    threads <- forM vars (\var -> forkIO (do
+        val <- takeMVar var
+        putMVar won val))
+    winner <- takeMVar won
+    forM threads killThread
+    return winner
+
+-- These workers simply return their name when they're done doing work.
+-- Guess they're more concerned with apparances than results!
+worker name result = do
+    -- Not all work is predictable!
+    delay <- randomRIO (50, 100)
+    sleepMs delay
+    putMVar result name
--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,14 @@ module Main where
 
 import qualified Ex1Threads (main)
 import qualified Ex2MVars (main)
+import qualified Ex5Select (main)
 
 -- | The main entry point.
 main :: IO ()
 main = do
     --example "Threads" Ex1Threads.main
-    example "MVar" Ex2MVars.main
+    --example "MVar" Ex2MVars.main
+    example "Select" Ex5Select.main
 
 example title code = do
     putStrLn ("~~~ Running " ++ title ++ " example! ~~~")
