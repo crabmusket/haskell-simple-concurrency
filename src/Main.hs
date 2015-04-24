2772309359af9f71182eeccb011ff0cab3c82f07
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-04-24 13:45:23 +1000

    Remove select, timeouts, and composable select. They're too Go-focused!

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,6 @@ import qualified Ex1Threads (main)
 import qualified Ex2MVars (main)
 import qualified Ex3Channels (main)
 import qualified Ex4DuplicatingChannels (main)
-import qualified Ex5Select (main)
-import qualified Ex6Timeouts (main)
-import qualified Ex7ComposableSelect (main)
 
 -- The main entry point.
 main :: IO ()
@@ -16,9 +13,6 @@ main = do
     example "MVar" Ex2MVars.main
     example "Channels" Ex3Channels.main
     example "Duplicating channels" Ex4DuplicatingChannels.main
-    example "Select" Ex5Select.main
-    example "Timeouts" Ex6Timeouts.main
-    example "Composable select" Ex7ComposableSelect.main
 
 example title code = do
     putStrLn ("~~~ Running " ++ title ++ " example! ~~~")
